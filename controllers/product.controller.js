95e76dd81886998aa9766099da7a95cdc1bcb287
krishnapavan <krishnapavan94@gmail.com>
2019-12-21 01:15:54 -0500

    initial commit

---
+++ controllers/product.controller.js
@@ -0,0 +1,24 @@
+const Product = require('../models/product.model');
+
+//Simple version, without validation or sanitation
+exports.test = function (req, res) {
+    res.send('Greetings from the Test controller!');
+};
+
+exports.product_create = function (req, res) {
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Note."
+            });
+        }
+        res.send('Product Created successfully')
+    })
+};
