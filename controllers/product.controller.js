9512fff3e3200e60632163e59b1c44dc42dfb8e8
krishnapavan <krishnapavan94@gmail.com>
2019-12-21 01:27:13 -0500

    added code

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -22,3 +22,25 @@ exports.product_create = function (req, res) {
         res.send('Product Created successfully')
     })
 };
+
+exports.product_update = function (req, res) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "Some error occurred while updating the Note."
+            });
+        }
+        res.send('Product udpated.');
+    });
+};
+
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "Some error occurred while updating the Note."
+            });
+        }
+        res.send('Deleted successfully!');
+    })
+};
